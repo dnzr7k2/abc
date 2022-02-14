5da0d46ac0b8b46843335064f909556e6671eb64
dnzr7k2 <99692263+dnzr7k2@users.noreply.github.com>
2022-02-15 05:42:39 +0800

    Create abc.sol

---
+++ abc.sol
@@ -0,0 +1,18 @@
+mapping(address => bool) public isAdmin;
+address public ownerAddress;
+
+modifier adminOnly{
+    require(abc.isAdmin[msg.sender]);
+    _;
+}
+
+modifier ownerOnly{
+    require(msg.sender==abc.ownerAddress);
+    _;
+}
+
+function setOwner(address _owner) public { // call in constructor
+    isAdmin[msg.sender]=true;
+    ownerAddress=_owner;
+    isAdmin[abc.ownerAddress]=true;
+}
